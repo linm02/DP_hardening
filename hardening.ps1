196db42a7741570d05c45102db7f1e228d9f16f8
linm02 <linm02@vse.cz>
2022-04-23 22:19:25 +0200

    audit policy init

--- hardening.ps1
+++ hardening.ps1
@@ -6,44 +6,48 @@
 #Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\System -Name DontDisplayNetworkSelectionUI -ErrorAction "SilentlyContinue"
 # Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\System -Name DontDisplayNetworkSelectionUI 2>$null
 
-$data = Import-Csv -Path .\data.csv
+$files = ".\data.csv",".\test.csv",".\bigmoney.csv"
 
+# ForEach ($file in $files) {
+$files | ForEach-Object {
+	# $data = Import-Csv -Path .\data.csv
+	$data = Import-Csv -Path "$_"
 
-$data | ForEach-Object {
-	
-	$RegPath = "$($_.hive):\$($_.path)"
-
-	# $ValueName = "DontDisplayNetworkSelectionUIs"
-	$ValueName = "$($_.value)"
-
-	$ValueData = "$($_.data)"
-	$ValueType = "$($_.type)"
-
-	#TODO pridat check jestli ma srpavny registry type??
-
-	if (Test-Path "$RegPath") {
-		$LastKey = Get-Item -LiteralPath $RegPath
+	$data | ForEach-Object {
 		
-		$CurrentValueData = $LastKey.GetValue($ValueName)
-		
-		if ($CurrentValueData -ne $null) {
-			"exists"
-			if ($CurrentValueData -ne $ValueData) {
-				"wrong value"
-				Set-ItemProperty -Path "$RegPath" -Name "$ValueName" -Value "$ValueData"
+		$RegPath = "$($_.hive):\$($_.path)"
+
+		# $ValueName = "DontDisplayNetworkSelectionUIs"
+		$ValueName = "$($_.value)"
+
+		$ValueData = "$($_.data)"
+		$ValueType = "$($_.type)"
+
+		#TODO pridat check jestli ma srpavny registry type??
+
+		if (Test-Path "$RegPath") {
+			$LastKey = Get-Item -LiteralPath $RegPath
+			
+			$CurrentValueData = $LastKey.GetValue($ValueName)
+			
+			if ($CurrentValueData -ne $null) {
+				"exists"
+				if ($CurrentValueData -ne $ValueData) {
+					"wrong value"
+					Set-ItemProperty -Path "$RegPath" -Name "$ValueName" -Value "$ValueData"
+				}
+			} else {
+				"does not exist - no value"
+				New-ItemProperty -Path "$RegPath" -Name "$ValueName" -PropertyType "$ValueType" -Value "$ValueData"
 			}
 		} else {
-			"does not exist - no value"
+			"Path does not exist"
+			#create the path !!!
+			New-Item -Path "$RegPath" -Force
 			New-ItemProperty -Path "$RegPath" -Name "$ValueName" -PropertyType "$ValueType" -Value "$ValueData"
 		}
-	} else {
-		"Path does not exist"
-		#create the path !!!
-		New-Item -Path "$RegPath" -Force
-		New-ItemProperty -Path "$RegPath" -Name "$ValueName" -PropertyType "$ValueType" -Value "$ValueData"
 	}
 }
-
 
 
 	
