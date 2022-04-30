dfd465c9b0c453ce9f0da4507e6825796eacfd86
linm02 <linm02@vse.cz>
2022-04-30 23:48:22 +0200

    putting it together

--- test-reg-delete.ps1
+++ test-reg-delete.ps1
@@ -5,3 +5,4 @@
 #EnableInvocationHeader
 Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription" -Name "EnableInvocationHeader"
 
+#New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "DisableLGPOProcessing" -PropertyType "DWord" -Value "1"
